b0030a5f166cbb0e5964af11567d5f32220c2eca
Cole Nelson <ctnelson1997@gmail.com>
2025-06-15 16:24:41 -0500

    clear index.js

--- starter/index.js
+++ starter/index.js
@@ -6,21 +6,6 @@
 //  https://cs571api.cs.wisc.edu/rest/su25/ice/pizza
 
 const reviewNum = 0;
-const REVIEWS = [
-    "A burst of warmth and flavor in every spoonful; simple yet irresistible!",
-    "The perfect blend of spice and comfort, an easy go-to chili recipe.",
-    "Loved the hearty texture and rich taste - a new family favorite!",
-    "Quick, flavorful, and satisfying - this chili hits all the right notes!"
-];
-
-// Gathers the original amounts of each ingredient.
-let ingrs = document.getElementById("ingredients-body").getElementsByTagName("tr");
-let originalAmounts = [];
-for (let ingr of ingrs) {
-    let amn = ingr.getElementsByTagName("td")[0].innerText;
-    let amnNum = parseFloat(amn);
-    originalAmounts.push(amnNum);
-}
 
 // TODO Implement the update yield!
 function updateYield() {
