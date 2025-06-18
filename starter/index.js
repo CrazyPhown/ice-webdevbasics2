9baa0083bc8006c309bc6a52dc36b33b12d83abd
Varun Putcha <vputcha@wisc.edu>
2025-06-18 18:23:49 -0500

    Fetching Data complete

--- starter/index.js
+++ starter/index.js
@@ -7,12 +7,34 @@
 
 const reviewNum = 0;
 
+fetch('https://cs571api.cs.wisc.edu/rest/su25/ice/chili', {
+	headers: {
+		'X-CS571-ID': CS571.getBadgerId(),
+	},
+})
+	.then((response) => {
+		//Callback function
+		console.log(response.status);
+		if (response.status !== 200) {
+			throw new error();
+		} else {
+			return response.json();
+		}
+	})
+	.then((data) => {
+		console.log(data);
+		// TODO: Insert the name into the webpage, insert the data into the webpage
+	})
+	.catch((e) => {
+		alert('Oops! invalid badger id');
+	});
+
 // TODO Implement the update yield!
 function updateYield() {
-    alert("I should update the yield!");
+	alert('I should update the yield!');
 }
 
 // TODO Implement the display review!
 function displayReview() {
-    alert("I should display a review!");
+	alert('I should display a review!');
 }
